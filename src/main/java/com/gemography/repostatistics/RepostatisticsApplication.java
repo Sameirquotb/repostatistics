e7ab275098cb463db07eb6b0f0ed6b7ca21d66f4
Sameir <squotb@gitprime.com>
2020-11-25 15:57:30 +0200

    initial commit

---
+++ src/main/java/com/gemography/repostatistics/RepostatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.gemography.repostatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepostatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepostatisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gemography/repostatistics/RepostatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gemography.repostatistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepostatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
